da8860b44acc19b82fadd51bb9e419275655810a
juvt1413 <m.aldossari210@gmail.com>
2022-11-10 21:10:28 +0200

    assignment5-part2 complete, first commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c5889fa45b09896ea69c7081d8ffbaeaf9cf6ba1
+AESD_ASSIGNMENTS_VERSION = e0a44f5b2698780065a4755c049410e990c4af23
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,6 +16,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -26,6 +27,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/	
+	
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 	
--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,4 @@ set -e
 cd `dirname $0`
 cd buildroot/
 
-make make AESD_ASSIGNMENTS_OVERRIDE_SRCDIR=/home/dawsari/Documents/ECEN_5713/assignments/assign_1_ssh/assignment-1-juvt1413 aesd-assignments-reconfigure
+make make aesd-assignments-rebuild #AESD_ASSIGNMENTS_OVERRIDE_SRCDIR=/home/dawsari/Documents/ECEN_5713/assignments/assign_1_ssh/assignment-1-juvt1413 
